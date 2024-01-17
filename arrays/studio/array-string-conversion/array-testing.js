abd8d32f52861b9270bc6654bc87b7189fc9abe5
Colin Brock <colinebrock@gmail.com>
2024-01-17 14:28:15 -0600

    Setting up workflows

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,6 +5,10 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+protoArray1.includes(",")
+protoArray2.includes(",")
+protoArray3.includes(",")
+protoArray4.includes(",")
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
